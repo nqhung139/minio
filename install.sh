0fa3339fadb2fc719969ad40f544a5dde157b52f
hung.nguyen <hung.nguyen@desoft.vn>
2025-02-10 17:45:16 +0700

    update code

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-helm upgrade --install --values ./values.yaml minio ./minio -n lake --create-namespace
+# helm upgrade --install --values ./values.yaml minio ./minio -n lake --create-namespace
+helm upgrade --install -f ./values.yaml  minio ./minio -n lake --create-namespace
 
 # curl -L -o repo.tar.gz https://github.com/nqhung139/minio/archive/refs/heads/main.tar.gz
 # tar -xzf repo.tar.gz
